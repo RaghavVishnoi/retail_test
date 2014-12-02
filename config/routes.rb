3a3a72d88f1f1426fb2c84afa8ee58761357c46b
mohit <>
2014-12-02 18:27:09 +0530

    screen json

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => { :format => :json }, :constraints => {:format => :json} do
     namespace :v1 do
+      get '/screens' => "screens#index"
       post '/users/sign_in' => 'sessions#create'
       delete '/users/sign_out' => 'sessions#destroy'
       resources :customers
