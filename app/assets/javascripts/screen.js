0da23aacbbb441a0ed4d26f6828f50da3a2cc05f
mohit <>
2015-02-09 12:53:34 +0530

    fix element-id

--- app/assets/javascripts/screen.js
+++ app/assets/javascripts/screen.js
@@ -5,8 +5,8 @@ $(document).on('submit', '#screen-form', function() {
 $(document).on('click', '.generate-element-id', function() {
   var unique_key = generate_unique_key();
   var $container = $(this).closest('.container');
-  var $element_id = $container.find('.element-id');
-  var $element_id_field = $container.find('.element-id-field')
+  var $element_id = $container.find('.element-id').first();
+  var $element_id_field = $container.find('.element-id-field').first();
   $element_id.text(unique_key);
   $element_id_field.val(unique_key);
 });
