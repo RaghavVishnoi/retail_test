7a9ee75d68a06cbf74c3f7305a40bca46df00218
mohit <>
2015-04-15 12:29:24 +0530

    changes in request views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,18 @@ module ApplicationHelper
     actions = Permission::Actions.map { |k, v| [v[:group_name], v[:permissions].invert.to_a] }
     grouped_options_for_select(actions, action_permission)
   end
+
+  def request_status_class(status)
+    params[:q] && params[:q][:status] == status ? 'active' : ''
+  end
+
+  def request_type_name(request)
+    if request.sis?
+      "SIS"
+    elsif request.gsb?
+      "GSB"
+    elsif request.in_shop?
+      "In-shop"
+    end
+  end
 end
