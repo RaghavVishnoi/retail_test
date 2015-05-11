c74082adef7e233320c07417d5b1d6e76b11902f
mohit <>
2015-05-11 17:55:47 +0530

    fix csv

--- app/services/requests_csv.rb
+++ app/services/requests_csv.rb
@@ -22,7 +22,7 @@ class RequestsCsv
   end
 
   def file_name
-    "#{Time.current.to_s}"
+    @file_name ||= "#{Time.current.to_i}"
   end
 
   def file_path
@@ -55,7 +55,7 @@ class RequestsCsv
       'City', 'state', 'Shop Name', 'Shop Address', 'Shop Owner Name', 'Shop Owner Phone', 'Avg. Store Monthly Sales',
       'Avg. Gionee Monthly Sales', 'Space Available in Store', 'Gionee GSB Present?', 'Type of SIS required?',
       'Is Gionee SIS installed in shop?', 'Is it main signage?', 'GSB installed outside?', 'Width', 'Height',
-      'Type of GSB Requested?', 'Shop Requirements', branding_details_header, 'Remarks'
+      'Type of GSB Requested?', 'Shop Requirements', branding_details_header, 'Remarks', 'status'
     ].flatten.join(',')
   end
 
@@ -74,7 +74,7 @@ class RequestsCsv
       request.shop_owner_name, request.shop_owner_phone, monthly_sales_str(request.avg_store_monthly_sales, AVG_STORE_MONTHLY_SALES), monthly_sales_str(request.avg_gionee_monthly_sales, AVG_GIONEE_MONTHLY_SALES),
       request.space_available, request.is_gionee_gsb_present, request.type_of_sis_required, request.is_sis_installed, request.is_main_signage,
       request.is_gsb_installed_outside, request.width, request.height, request.type_of_gsb_requested, field_values_str(request.shop_requirements),
-      branding_details_csv(request), request.remarks
+      branding_details_csv(request), request.remarks, request.status
     ].flatten.join(',')
   end
 
