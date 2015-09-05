9081cefa67162e321bcc677f1546d2a3a4430d72
raghav.pratap <raghavvishnoi10@gmail.com>
2015-09-05 12:09:30 +0530

    update changes

--- app/controllers/api/v1/retailerlists_controller.rb
+++ app/controllers/api/v1/retailerlists_controller.rb
@@ -6,7 +6,9 @@ module Api
         def index
     		value = params[:value]
         arr = [:retailer_code => '*Select Retailer Code']
+
 			  @city = arr+Retailer.where(city: value,status: 'Active').select(:retailer_code,:retailer_name).order("lfr_chain desc")
+
         render :json => @city
     	end
      end
