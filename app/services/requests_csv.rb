4d1941210bd6606356c7b9754a1a9ed85f8884e7
raghav.pratap <raghavvishnoi10@gmail.com>
2016-01-18 07:53:42 +0530

    change requests_csv

--- app/services/requests_csv.rb
+++ app/services/requests_csv.rb
@@ -129,89 +129,89 @@ class RequestsCsv
   end
 
   def to_csv(request)
-    retailer_code = request.retailer_code.upcase
-    @retailer = Retailer.find_by(:retailer_code => retailer_code)
+      retailer_code = request.retailer_code
+      @retailer = Retailer.find_by(:retailer_code => retailer_code)
 
-    if request.city == '' && @retailer != nil || request.city == nil && @retailer != nil
-       @retailer_city = @retailer.city
-    else
-      @retailer_city = request.city
-    end
+      if request.city == '' && @retailer != nil || request.city == nil && @retailer != nil
+         @retailer_city = @retailer.city
+      else
+        @retailer_city = request.city
+      end
 
-    if request.state == '' && @retailer != nil || request.state == nil && @retailer != nil
-      @retailer_state = @retailer.state
-    else
-      @retailer_state = request.state
-    end
+      if request.state == '' && @retailer != nil || request.state == nil && @retailer != nil
+        @retailer_state = @retailer.state
+      else
+        @retailer_state = request.state
+      end
 
-    if request.shop_name == '' && @retailer != nil || request.shop_name == nil && @retailer != nil
-      @shop_name = @retailer.retailer_name
-    else
-      @shop_name = request.shop_name
-    end
+      if request.shop_name == '' && @retailer != nil || request.shop_name == nil && @retailer != nil
+        @shop_name = @retailer.retailer_name
+      else
+        @shop_name = request.shop_name
+      end
 
-    if request.shop_owner_name == '' && @retailer != nil || request.shop_owner_name == nil && @retailer != nil 
-     @shop_owner_name = @retailer.contact_person
-    else
-     @shop_owner_name = request.shop_owner_name
-    end
+      if request.shop_owner_name == '' && @retailer != nil || request.shop_owner_name == nil && @retailer != nil 
+       @shop_owner_name = @retailer.contact_person
+      else
+       @shop_owner_name = request.shop_owner_name
+      end
 
-    if request.shop_owner_phone == '' && @retailer != nil || request.shop_owner_phone == nil && @retailer != nil
-     @shop_owner_phone = @retailer.mobile_number
-    else
-     @shop_owner_phone = request.shop_owner_phone  
-    end
+      if request.shop_owner_phone == '' && @retailer != nil || request.shop_owner_phone == nil && @retailer != nil
+       @shop_owner_phone = @retailer.mobile_number
+      else
+       @shop_owner_phone = request.shop_owner_phone  
+      end
 
-    if request.shop_address == '' && @retailer != nil || request.shop_address == nil && @retailer != nil
-     @shop_address = @retailer.address
-    else
-     @shop_address = request.shop_address
-    end
+      if request.shop_address == '' && @retailer != nil || request.shop_address == nil && @retailer != nil
+       @shop_address = @retailer.address
+      else
+       @shop_address = request.shop_address
+      end
 
 
-    
-    if @request_type == 'All' || @request_type == '' || @request_type == nil
-      [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code, request.rsp_name,
-      request.rsp_mobile_number, request.rsp_app_user_id,@retailer_city, @retailer_state, @shop_name, @shop_address,
-      @shop_owner_name, @shop_owner_phone, monthly_sales_str(request.avg_store_monthly_sales, AVG_STORE_MONTHLY_SALES), monthly_sales_str(request.avg_gionee_monthly_sales, AVG_GIONEE_MONTHLY_SALES),
-      request.space_available, request.is_gionee_gsb_present, request.type_of_sis_required, request.is_sis_installed, request.is_main_signage,
-      request.is_gsb_installed_outside, request.width, request.height, request.type_of_gsb_requested, field_values_str(request.shop_requirements),
-      branding_details_csv(request), request.remarks, request.comment_by_approver,request.comment_by_cmo,  request.created_at.strftime("%b %d, %Y"),request.type_of_issue,request.type_of_problem,
-      request.shop_visit_date,request.shop_visit_done_by,request.is_standee_present,request.visitor_contact_number,request.store_selling_gionee,request.is_clipon_present,request.is_countertop_present,request.is_leaflets_available,
-      request.no_of_peace_in_stock,request.is_wall_poster_in_shop,request.is_dangler_in_shop,request.rsp_assigned_in_store,request.rsp_present_in_shop,request.rsp_in_gionee_tshirt,request.rsp_well_groomed,request.rsp_selling_skills,request.gsb_type_installed,request.location_of_gsb,
-      request.gsb_cleanliness,request.installation_quality,request.is_gsb_light_woring,request.is_gsb_light_throw_is_good,request.gsb_structured_damage,request.gsb_other_problem,request.gsb_retailer_feedback,request.is_sis_present,request.is_sis_placed_properly,request.is_sis_condition_good,
-      request.is_sis_cleaned_daily,request.is_sis_damaged,request.sis_structured_flaws,request.sis_security_alarm_working,request.sis_security_device_charging,request.sis_demo_phones_installed,request.spec_card_demo_phone_match,request.backwall_light_working_properly,
-      request.is_counter_lights_working,request.is_clip_on_lights,request.dealer_switch_on_sis_lights,request.updated_gionee_creative,request.sis_any_problem,request.sis_retailer_feedback,request.is_good_visibility_in_store,request.lit_in_store,request.has_a_relevant_visual,
-      request.overall_rating,request.is_clipon_not_working_properly,request.overall_comments
-      ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
-
-     elsif @request_type == 'GSB' || @request_type == 'SIS' || @request_type == 'InShop'
-      [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code, request.rsp_name,
-      request.rsp_mobile_number, request.rsp_app_user_id,@retailer_city, @retailer_state, @shop_name, @shop_address,
-      @shop_owner_name, @shop_owner_phone, monthly_sales_str(request.avg_store_monthly_sales, AVG_STORE_MONTHLY_SALES), monthly_sales_str(request.avg_gionee_monthly_sales, AVG_GIONEE_MONTHLY_SALES),
-      request.space_available, request.is_gionee_gsb_present, request.type_of_sis_required, request.is_sis_installed, request.is_main_signage,
-      request.is_gsb_installed_outside, request.width, request.height, request.type_of_gsb_requested, field_values_str(request.shop_requirements),
-      branding_details_csv(request), request.remarks, request.comment_by_approver, request.comment_by_cmo,  request.created_at.strftime("%b %d, %Y")
-      ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
-
-     elsif @request_type == 'Maintenance'
-      [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code,@retailer_city, @retailer_state, @shop_name, @shop_address,
-      @shop_owner_name, @shop_owner_phone, request.remarks, request.comment_by_approver, request.comment_by_cmo, 
-       request.created_at.strftime("%b %d, %Y"),request.type_of_issue,request.type_of_problem
-      ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
-
-     elsif @request_type == 'Audit'
-     [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code, request.remarks, request.comment_by_approver, request.comment_by_cmo, 
-       request.created_at.strftime("%b %d, %Y"),request.shop_visit_date,request.shop_visit_done_by,request.is_standee_present,request.visitor_contact_number,request.store_selling_gionee,request.is_clipon_present,request.is_countertop_present,request.is_leaflets_available,
-       request.no_of_peace_in_stock,request.is_wall_poster_in_shop,request.is_dangler_in_shop,request.rsp_assigned_in_store,request.rsp_present_in_shop,request.rsp_in_gionee_tshirt,request.rsp_well_groomed,request.rsp_selling_skills,request.gsb_type_installed,request.location_of_gsb,
-       request.gsb_cleanliness,request.installation_quality,request.is_gsb_light_woring,request.is_gsb_light_throw_is_good,request.gsb_structured_damage,request.gsb_other_problem,request.gsb_retailer_feedback,request.is_sis_present,request.is_sis_placed_properly,request.is_sis_condition_good,
-       request.is_sis_cleaned_daily,request.is_sis_damaged,request.sis_structured_flaws,request.sis_security_alarm_working,request.sis_security_device_charging,request.sis_demo_phones_installed,request.spec_card_demo_phone_match,request.backwall_light_working_properly,
-       request.is_counter_lights_working,request.is_clip_on_lights,request.dealer_switch_on_sis_lights,request.updated_gionee_creative,request.sis_any_problem,request.sis_retailer_feedback,request.is_good_visibility_in_store,request.lit_in_store,request.has_a_relevant_visual,
-       request.overall_rating,request.is_clipon_not_working_properly,field_values_str(request.shop_requirements),branding_details_csv(request),request.overall_comments
-      ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
       
-     end
-    
+      if @request_type == 'All' || @request_type == '' || @request_type == nil
+        [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code, request.rsp_name,
+        request.rsp_mobile_number, request.rsp_app_user_id,@retailer_city, @retailer_state, @shop_name, @shop_address,
+        @shop_owner_name, @shop_owner_phone, monthly_sales_str(request.avg_store_monthly_sales, AVG_STORE_MONTHLY_SALES), monthly_sales_str(request.avg_gionee_monthly_sales, AVG_GIONEE_MONTHLY_SALES),
+        request.space_available, request.is_gionee_gsb_present, request.type_of_sis_required, request.is_sis_installed, request.is_main_signage,
+        request.is_gsb_installed_outside, request.width, request.height, request.type_of_gsb_requested, field_values_str(request.shop_requirements),
+        branding_details_csv(request), request.remarks, request.comment_by_approver,request.comment_by_cmo,  request.created_at.strftime("%b %d, %Y"),request.type_of_issue,request.type_of_problem,
+        request.shop_visit_date,request.shop_visit_done_by,request.is_standee_present,request.visitor_contact_number,request.store_selling_gionee,request.is_clipon_present,request.is_countertop_present,request.is_leaflets_available,
+        request.no_of_peace_in_stock,request.is_wall_poster_in_shop,request.is_dangler_in_shop,request.rsp_assigned_in_store,request.rsp_present_in_shop,request.rsp_in_gionee_tshirt,request.rsp_well_groomed,request.rsp_selling_skills,request.gsb_type_installed,request.location_of_gsb,
+        request.gsb_cleanliness,request.installation_quality,request.is_gsb_light_woring,request.is_gsb_light_throw_is_good,request.gsb_structured_damage,request.gsb_other_problem,request.gsb_retailer_feedback,request.is_sis_present,request.is_sis_placed_properly,request.is_sis_condition_good,
+        request.is_sis_cleaned_daily,request.is_sis_damaged,request.sis_structured_flaws,request.sis_security_alarm_working,request.sis_security_device_charging,request.sis_demo_phones_installed,request.spec_card_demo_phone_match,request.backwall_light_working_properly,
+        request.is_counter_lights_working,request.is_clip_on_lights,request.dealer_switch_on_sis_lights,request.updated_gionee_creative,request.sis_any_problem,request.sis_retailer_feedback,request.is_good_visibility_in_store,request.lit_in_store,request.has_a_relevant_visual,
+        request.overall_rating,request.is_clipon_not_working_properly,request.overall_comments
+        ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
+
+       elsif @request_type == 'GSB' || @request_type == 'SIS' || @request_type == 'InShop'
+        [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code, request.rsp_name,
+        request.rsp_mobile_number, request.rsp_app_user_id,@retailer_city, @retailer_state, @shop_name, @shop_address,
+        @shop_owner_name, @shop_owner_phone, monthly_sales_str(request.avg_store_monthly_sales, AVG_STORE_MONTHLY_SALES), monthly_sales_str(request.avg_gionee_monthly_sales, AVG_GIONEE_MONTHLY_SALES),
+        request.space_available, request.is_gionee_gsb_present, request.type_of_sis_required, request.is_sis_installed, request.is_main_signage,
+        request.is_gsb_installed_outside, request.width, request.height, request.type_of_gsb_requested, field_values_str(request.shop_requirements),
+        branding_details_csv(request), request.remarks, request.comment_by_approver, request.comment_by_cmo,  request.created_at.strftime("%b %d, %Y")
+        ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
+
+       elsif @request_type == 'Maintenance'
+        [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code,@retailer_city, @retailer_state, @shop_name, @shop_address,
+        @shop_owner_name, @shop_owner_phone, request.remarks, request.comment_by_approver, request.comment_by_cmo, 
+         request.created_at.strftime("%b %d, %Y"),request.type_of_issue,request.type_of_problem
+        ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
+
+       elsif @request_type == 'Audit'
+       [ request.id,request.status, request_type_name(request),request.is_rsp, request.rsp_not_present_reason, request.cmo.try(:display_name), request.retailer_code, request.remarks, request.comment_by_approver, request.comment_by_cmo, 
+         request.created_at.strftime("%b %d, %Y"),request.shop_visit_date,request.shop_visit_done_by,request.is_standee_present,request.visitor_contact_number,request.store_selling_gionee,request.is_clipon_present,request.is_countertop_present,request.is_leaflets_available,
+         request.no_of_peace_in_stock,request.is_wall_poster_in_shop,request.is_dangler_in_shop,request.rsp_assigned_in_store,request.rsp_present_in_shop,request.rsp_in_gionee_tshirt,request.rsp_well_groomed,request.rsp_selling_skills,request.gsb_type_installed,request.location_of_gsb,
+         request.gsb_cleanliness,request.installation_quality,request.is_gsb_light_woring,request.is_gsb_light_throw_is_good,request.gsb_structured_damage,request.gsb_other_problem,request.gsb_retailer_feedback,request.is_sis_present,request.is_sis_placed_properly,request.is_sis_condition_good,
+         request.is_sis_cleaned_daily,request.is_sis_damaged,request.sis_structured_flaws,request.sis_security_alarm_working,request.sis_security_device_charging,request.sis_demo_phones_installed,request.spec_card_demo_phone_match,request.backwall_light_working_properly,
+         request.is_counter_lights_working,request.is_clip_on_lights,request.dealer_switch_on_sis_lights,request.updated_gionee_creative,request.sis_any_problem,request.sis_retailer_feedback,request.is_good_visibility_in_store,request.lit_in_store,request.has_a_relevant_visual,
+         request.overall_rating,request.is_clipon_not_working_properly,field_values_str(request.shop_requirements),branding_details_csv(request),request.overall_comments
+        ].flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
+        
+       end
+      
     
   end
 
