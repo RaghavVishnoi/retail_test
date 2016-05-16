cd57976bf5f2af6fd491e44cb2bb2bcc906f6512
raghav.pratap <raghavvishnoi10@gmail.com>
2016-05-16 11:30:48 +0530

    update map

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,26 +1,22 @@
 class Map < ActiveRecord::Base
-  REQUEST_STATUS = ['All', 'CMO Pending', 'CMO Declined', 'Pending', 'Approved', 'Declined']
-  REQUEST_TYPE = ['All','GSB','In-shop','SIS','Maintenance','Audit']
-  STORE_SIZE = ['0-10','10-20','20-30','30-40','40-50']
-  GIONEE_SALES = ['10-50K','50K-1Lac','1Lac-5Lac','5Lac-10Lac','More than 10Lac']
   
   belongs_to :images
   belongs_to :requests
 
   validate :status
 
-  def self.image_data(params)
+  def self.image_data(params,current_user)
       @from = params[:from_date].to_s
       @to = params[:till_date].to_s
       @state = params[:state]
       
       if @from == '0' && @to == '0'
         @to = Time.now.to_date.to_s
-        @from = (Time.now - 1.month).to_date.to_s
+        @from = (Time.now - 1.day).to_date.to_s
       end
 
       if @state.include?('All State')
-        @states = State.all.pluck(:id)
+        @states = State.states(current_user)
       else
         @states = @state.map!(&:to_i)
       end
@@ -34,15 +30,14 @@ class Map < ActiveRecord::Base
         status = @status
       end
       status = status.flatten.map {|v| "\"#{v.to_s.gsub('"', '""')}\"" }.join(',')
-
-          @request_type = @request_type.map!(&:to_i)  
-          request_type = @request_type.join(',')
-          @from_date = Time.parse(@from).to_date.beginning_of_day
-          @till_date = Time.parse(@to).to_date.end_of_day
-          
-          sql = "select rq.request_type,rq.status,rq.state,im.lat,im.long,rq.id from  requests as rq JOIN  images as im ON rq.id= im.imageable_id where  rq.state_id IN (#{states}) AND  rq.created_at  BETWEEN '#{@from_date}' AND '#{@till_date}' AND rq.request_type IN(#{request_type}) AND rq.status  IN(#{status}) ;" 
-          response = ActiveRecord::Base.connection.execute(sql)
-          
-          response            
+      @request_type = @request_type.map!(&:to_i)  
+      request_type = @request_type.join(',')
+      @from_date = Time.parse(@from).to_date.beginning_of_day
+      @till_date = Time.parse(@to).to_date.end_of_day
+      
+      sql = "select rq.request_type,rq.status,rq.state,im.lat,im.long,rq.id from  requests as rq JOIN  images as im ON rq.id= im.imageable_id where  rq.state_id IN (#{states}) AND  rq.created_at  BETWEEN '#{@from_date}' AND '#{@till_date}' AND rq.request_type IN(#{request_type}) AND rq.status  IN(#{status}) ;" 
+      response = ActiveRecord::Base.connection.execute(sql)
+      
+      response            
   end
 end
