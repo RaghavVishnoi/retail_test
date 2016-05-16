514dbb3b815e79ec9092e784ca7a3f04f2216415
raghav.pratap <raghavvishnoi10@gmail.com>
2016-05-16 11:40:24 +0530

    update states

--- app/models/state.rb
+++ app/models/state.rb
@@ -4,7 +4,7 @@ class State < ActiveRecord::Base
 	has_many :requests
 
 	def self.permit_state(user)
-		if ['superadmin','approver'].include? User.get_role(user.id)
+		if ['superadmin','approver','vmqa'].include? User.get_role(user.id)
 			State.all.pluck(:name,:id)
 		else
 			state_id = user.user_states.pluck(:state_id)	
@@ -13,7 +13,7 @@ class State < ActiveRecord::Base
 	end
 
 	def self.state_with_name(user)
-		if ['superadmin','approver'].include? User.get_role(user.id)
+		if ['superadmin','approver','vmqa'].include? User.get_role(user.id)
 			State.all.pluck(:name,:name)
 		else
 			state_id = user.user_states.pluck(:state_id)
@@ -22,11 +22,20 @@ class State < ActiveRecord::Base
 	end
 
 	def self.states(user)
-		if ['superadmin','approver'].include? User.get_role(user.id)
-			State.all.pluck(:id)
+ 		if ['superadmin','approver','vmqa'].include? User.get_role(user.id)
+  			State.all.pluck(:id)
 		else
 			state_id = user.user_states.pluck(:state_id)
 			State.where(id: state_id).pluck(:id)
+		end
+	end
+
+	def self.states_name(user)
+		if ['superadmin','approver','vmqa'].include? User.get_role(user.id)
+  			State.all.pluck(:name)
+		else
+			state_id = user.user_states.pluck(:state_id)
+			State.where(id: state_id).pluck(:name)
 		end
 	end
 
