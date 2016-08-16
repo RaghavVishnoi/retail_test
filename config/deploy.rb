4b3b8291c2ee0d907b035d0578ed87108f1d6ad6
RaghavSingh <Raghav.Singh@lptpl.com>
2016-08-16 21:05:03 +0530

    update database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.6.0'
 
 set :application, 'fosem-staging'
 set :repo_url, 'ssh://xp-dev.com/LPTPL_GIONEE_REQUESTER_WEB'
-set :branch, :phase_6
+set :branch, :vendor_workflow
 set :tmp_dir, "/home/fosem/tmp"
 
 # Default branch is :master
