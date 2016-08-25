19701b66c31c188d17c8f65e8f813e6b25a4f0d0
RaghavSingh <Raghav.Singh@lptpl.com>
2016-08-25 23:38:52 +0530

    update migration

--- db/migrate/20160825044538_update_sales_column_into_shop_audits.rb
+++ db/migrate/20160825044538_update_sales_column_into_shop_audits.rb
@@ -1,11 +1,5 @@
 class UpdateSalesColumnIntoShopAudits < ActiveRecord::Migration
   def change
   	rename_column :shop_audits,:avg_monthly_sale,:average_monthly_sales
-  	rename_column :shop_audits,:most_selling_brand ,:most_selling_brand
-  	rename_column :shop_audits,:second_most_selling_brand ,:second_most_selling_brand
-  	rename_column :shop_audits,:third_most_selling_brand ,:third_most_selling_brand
-  	rename_column :shop_audits,:gionee_sales ,:gionee_sales
-  	rename_column :shop_audits,:gionee_stock_quantity ,:gionee_stock_quantity
-  	rename_column :shop_audits,:models_available ,:models_available
   end
 end
