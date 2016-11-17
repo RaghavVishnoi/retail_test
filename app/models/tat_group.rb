2cb3bb9bbdd461a50469b882a36c1308e7770418
RaghavSingh <Raghav.Singh@lptpl.com>
2016-11-17 12:11:08 +0530

    add exception handling to tat

--- app/models/tat_group.rb
+++ app/models/tat_group.rb
@@ -4,15 +4,19 @@ class TatGroup < ActiveRecord::Base
 	validates :duration, presence: true
 
 	def self.isTatLimit?(moduleName,lastUpdateDate)
-		if self.find_by(name: moduleName) != nil
-			if lastUpdateDate+self.find_by(name: moduleName).duration.days <= Time.now
-				true
+		begin
+			if self.find_by(name: moduleName) != nil
+				if lastUpdateDate+self.find_by(name: moduleName).duration.days <= Time.now
+					true
+				else
+					false
+				end
 			else
-				false
+				true
 			end
-		else
-			true
-		end
+		rescue
+			false
+		end	
 	end
 
 end
